124e4531469cb51edcf09ffe6debca8f89e8a9ea
Olga <olga.erkiert@protonmail.com>
2023-08-17 16:09:02 +0200

    Create basic GUI

---
+++ gui.py
@@ -0,0 +1,11 @@
+import functions
+import PySimpleGUI as gui
+
+
+label = gui.Text("Type in a to do")
+input_box = gui.InputText(tooltip="Enter to do")
+add_button = gui.Button("Add")
+
+window = gui.Window("To Do App", layout=[[label, input_box, add_button]])
+window.read()
+window.close()
